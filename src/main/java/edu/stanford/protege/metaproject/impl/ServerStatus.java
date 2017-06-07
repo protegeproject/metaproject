4e1dbbe678f2d7f12f4d75e30977a3fbf9553c9a
AnthonyMl <mladenov.anthony@gmail.com>
2017-06-07 11:11:38 -0400

    Change pause state to include pausing user for better handling of menu enabling

--- src/main/java/edu/stanford/protege/metaproject/impl/ServerStatus.java
+++ src/main/java/edu/stanford/protege/metaproject/impl/ServerStatus.java
@@ -1,5 +1,8 @@
 package edu.stanford.protege.metaproject.impl;
 
+import com.google.common.base.Optional;
+import edu.stanford.protege.metaproject.api.User;
+
 import javax.annotation.concurrent.Immutable;
 import java.io.Serializable;
 
@@ -7,9 +10,12 @@ import java.io.Serializable;
 @Immutable
 public final class ServerStatus implements Serializable {
 	private static final long serialVersionUID = -206877592564818670L;
-	public final boolean isPaused;
 
-	public ServerStatus(boolean isPaused) {
-		this.isPaused = isPaused;
+	public final Optional<User> pausingUser;
+
+	public ServerStatus(Optional<User> pausingUser) {
+		this.pausingUser = pausingUser;
 	}
+
+
 }
